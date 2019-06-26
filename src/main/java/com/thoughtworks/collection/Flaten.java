1a0be372269192850cd114c40c938335e8762329
yaeli <yaeli@thoughtworks.com>
2019-06-26 23:20:36 +0800

    [yae][implement Flaten class]

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,21 +1,31 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+
+        for (Integer[] oneArray : array) {
+            Collections.addAll(result, oneArray);
+        }
+        return result;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        Flaten flaten = new Flaten(array);
+        List<Integer> dimesionalArray = flaten.transformToOneDimesional();
+
+        Filter filter = new Filter(dimesionalArray);
+        return filter.getDifferentElements();
     }
 }
